6e42ff9845061d37cd9f5b39c121837e7019c2be
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-09-02 01:18:39 +0200

    Create functions.js

---
+++ JS/functions.js
@@ -0,0 +1,26 @@
+//// code that can be reused over and over again
+function logger(input){
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
