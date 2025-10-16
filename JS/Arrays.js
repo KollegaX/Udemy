b8c09c62f9cb87f36be649e6b1779d5a5b63a47c
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-10-16 11:02:55 +0200

    Update Arrays.js

--- JS/Arrays.js
+++ JS/Arrays.js
@@ -208,3 +208,19 @@ const {deposits, withdrawals} = accounts.flatMap(acc => acc.movements).reduce((s
 }, {deposits : 0, withdrawals: 0});
 console.log(deposits, withdrawals);
 
+
+///
+const convertTitleCase = function(title){
+    const capitalize = str => str[0].toUpperCase() + str.slice(1)
+    const exceptions = ['a','an','the','and','but','or','on','in','with'];
+
+    const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => exceptions.includes(word) ? word : capitalize(word)).join(' ')
+    return capitalize(titleCase);
+}
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is anothr title with an EXAMPLE'));
+
